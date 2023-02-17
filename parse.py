2d1257c40d9e7d085843f77f1735edf7bf5f59db
nvpost <nvbpost@yandex.ru>
2023-02-17 12:52:21 +0300

    white

--- parse.py
+++ parse.py
@@ -7,30 +7,34 @@ from io import BytesIO
 root = ET.parse('yml_feed.xml').getroot()
 
 сounter = 0
+limit = 57
 elements = len(root.find('shop/offers'))
 for i in root.find('shop/offers'):
-    # if(сounter<limit)
     сounter = сounter + 1
+    if(сounter<limit):
+        continue
 
     src = i.find('picture').text
 
-
-    filename = src.split('/')
-    # output_url = dir + "/" + filename
+    filename = src.split('/')[-1]
     r = requests.get(src)
-
+    print(filename)
 
     img = Image.open(BytesIO(r.content))
-    img = img.convert("RGBA")
+    if '.jpg' in filename:
+        print('это jpg')
+        new_img = img
+
+    else:
+        img = img.convert("RGBA")
+
+        overlay = Image.new('RGBA', img.size, (225, 225, 225, 0))
+        new_img = img = Image.alpha_composite(overlay, img)
+
+    new_img.save('imgs/'+filename)
 
 
-    overlay = Image.new('RGBA', img.size, (225, 225, 225))
-    new_img = img = Image.alpha_composite(overlay, img)
 
-    new_img.save('imgs/'+filename[-1])
-
-
-
-    print(filename, сounter, '/', elements)
+    print("сохранено", сounter, '/', elements)
     # if сounter > 1:
     #     break
