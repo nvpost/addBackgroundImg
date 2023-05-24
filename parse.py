97d4e8f3c355e04b170b63d8ffbc0f785233c2a4
nvpost <nvbpost@yandex.ru>
2023-05-24 12:07:56 +0300

    fix white

--- parse.py
+++ parse.py
@@ -4,23 +4,26 @@ import requests
 from io import BytesIO
 
 
-root = ET.parse('yml_feed.xml').getroot()
+root = ET.parse('feed.xml').getroot()
 
 сounter = 0
-limit = 57
+# limit = 57 Страртуем от 57
 elements = len(root.find('shop/offers'))
+no_aplfa = []
 for i in root.find('shop/offers'):
     сounter = сounter + 1
-    if(сounter<limit):
-        continue
+    # if(сounter<limit):
+    #     continue
 
     src = i.find('picture').text
 
     filename = src.split('/')[-1]
     r = requests.get(src)
-    print(filename)
+    # print(filename)
 
     img = Image.open(BytesIO(r.content))
+    img.thumbnail(size=(600, 600))
+
     if '.jpg' in filename:
         print('это jpg')
         new_img = img
@@ -28,13 +31,24 @@ for i in root.find('shop/offers'):
     else:
         img = img.convert("RGBA")
 
-        overlay = Image.new('RGBA', img.size, (225, 225, 225, 0))
-        new_img = img = Image.alpha_composite(overlay, img)
+        pix = img.load()
+        apl = pix[1, 1][3]
+        if apl !=0:
+            print(src)
+            no_aplfa.append(src)
 
-    new_img.save('imgs/'+filename)
+        overlay = Image.new('RGBA', img.size, '#ffffff')
 
+        new_img = Image.alpha_composite(overlay, img)
 
+    save_flag = new_img.save('imgs/'+filename)
+    if save_flag == False:
+        print('не сохранено', src)
+        break
 
-    print("сохранено", сounter, '/', elements)
+    print("сохранено", сounter, '/', elements, save_flag)
     # if сounter > 1:
     #     break
+
+
+print(len(no_aplfa))
